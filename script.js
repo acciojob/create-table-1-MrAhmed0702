693029ff9018ae1ee58321ec5892eef189477d60
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-10 21:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+	//Write your code here
+	let tble = document.getElementById("sampleTable");
+	let newRow = tble.insertRow(0);
+	let c1 = newRow.insertCell(0);
+	let c2 = newRow.insertCell(1);
+
+	c1.textContent = "New Cell1";
+	c2.textContent = "New Cell2";
 }
